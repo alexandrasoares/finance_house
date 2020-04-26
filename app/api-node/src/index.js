a76ed31a493eaa4aa45d77c56877a557377398d6
Soares Alexandra <al.soares@reply.com>
2020-04-26 10:18:01 -0300

    controller modulo

--- app/api-node/src/index.js
+++ app/api-node/src/index.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-require('./app/controller/autenticacaoController')(app);
-require('./app/controller/projetoController')(app);
+require('./app/controller/controller-module')(app);
 
 //porta da API
 app.listen('3000');
