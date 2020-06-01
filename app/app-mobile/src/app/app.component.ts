613d5abd1f54d7e9c8ff102e1d8f45e4fcf576f7
Soares Alexandra <al.soares@reply.com>
2020-05-31 22:53:54 -0300

    config build android

--- app/app-mobile/src/app/app.component.ts
+++ app/app-mobile/src/app/app.component.ts
@@ -23,8 +23,8 @@ export class AppComponent implements OnInit {
       icon: 'paper-plane'
     },
     {
-      title: 'Favorites',
-      url: '/folder/Favorites',
+      title: 'Criar conta',
+      url: '/folder/Criar conta',
       icon: 'heart'
     },
     {
