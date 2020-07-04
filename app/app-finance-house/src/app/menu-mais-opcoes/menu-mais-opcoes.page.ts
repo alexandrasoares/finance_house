a125aeb2652b0c5e3a7b7ff069b204c5c44d7b39
Soares Alexandra <al.soares@reply.com>
2020-07-03 22:28:06 -0300

    menu de mais opções

--- app/app-finance-house/src/app/menu-mais-opcoes/menu-mais-opcoes.page.ts
+++ app/app-finance-house/src/app/menu-mais-opcoes/menu-mais-opcoes.page.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-mais-opcoes.page.scss'],
 })
 export class MenuMaisOpcoesPage implements OnInit {
+  appPagesCadastros: any[] = [
+    {title: 'Contas', icon: 'bookmarks', link: '/contas'},
+    {title: 'Categorias', icon: 'pricetag', link: '/categorias'},
+    {title: 'Subcategorias', icon: 'pricetags', link: '/subcategorias'},
+    // {title: 'Tipos de conta', icon: 'document', link: '/tipos-conta'},
+    // {title: 'Projetos', icon: 'briefcase', link: '/projetos'},
+    {title: 'Beneficiários', icon: 'contacts', link: '/beneficiarios'},
+    // {title: 'Corretoras', icon: 'business', link: '/corretoras'}
+  ];
+
+  appPagesOutros: any[] = [
+    {title: 'Cobranças', icon: 'clipboard', link: '/cobrancas'},
+    // {title: 'Transferências', icon: 'repeat', link: '/transferencias'},
+    // {title: 'Investimentos', icon: 'analytics', link: '/investimentos'}
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  hasPagesCadastros(): boolean {
+    return this.appPagesCadastros.length > 0;
+  }
+
+  hasPagesOutros(): boolean {
+    return this.appPagesOutros.length > 0;
   }
 
 }
